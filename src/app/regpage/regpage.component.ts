5f2c804c1deb73119355c81be237dd660a0a3194
mutantsforever <rohit.97kushwaha@gmail.com>
2018-06-05 23:16:54 +0530

    additional reg validation

--- src/app/regpage/regpage.component.ts
+++ src/app/regpage/regpage.component.ts
@@ -15,7 +15,35 @@ export class RegpageComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      fname: new FormControl("", Validators.required)
+      fname: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      lname: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      emailid: new FormControl("", Validators.compose([
+        Validators.required,
+        // Validators.email,
+        // Validators.pattern("[^ @]*@[^ @]*"),
+        Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"),
+      ])),
+      mobno: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+'),
+        Validators.minLength(10),
+        Validators.maxLength(10),
+      ])),
+      pwd: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(8)
+      ])),
+      cpwd: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(8)
+      ])),
+      
     });
   }
   onClickSubmit() {
